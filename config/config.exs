8912caf4f3cf0bf2ffb597bdfffd430dbf187d05
Mike Stok <mstok@kineticcafe.com>
2022-12-06 11:15:24 -0500

    Only spin up repo process when we need it

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,4 @@
 import Config
 
-config :experiment, DynamicSqlite.Repo, database: :memory
-
 # Shrug. As usual Elixir is a series of increasingly annoying paper cuts.
 config :logger, level: :error
--- lib/dynamic_sqlite/repo.ex
+++ lib/dynamic_sqlite/repo.ex
@@ -5,8 +5,6 @@ defmodule DynamicSqlite.Repo do
 
 
   def using_file(filename, callback) do
-    default_dynamic_repo = get_dynamic_repo()
-
     start_opts = [name: nil, pool_size: 1, database: filename]
 
     {:ok, repo} = __MODULE__.start_link(start_opts)
@@ -15,7 +13,6 @@ defmodule DynamicSqlite.Repo do
       __MODULE__.put_dynamic_repo(repo)
       callback.()
     after
-      __MODULE__.put_dynamic_repo(default_dynamic_repo)
       Supervisor.stop(repo)
     end
   end
